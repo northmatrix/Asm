883ba3ac31d8b94820989babcaed31afea3d12b5
North <56787922+North004@users.noreply.github.com>
2024-12-11 23:20:37 +0000

    Update Main.asm

--- Main.asm
+++ Main.asm
@@ -1,15 +1,36 @@
+section .data
+    msg db "A DUNGEON GAME ABOUT CAVES",10,"MADE BY NORTH004 :)",10,10,0
+    msg1 db "YOU ARE A KNIGHT WITH",10,10,"HEALTH: 10",10,"STAMMINA: 10",10,"MAGIC: 10",10,0
 section .bss
-    string resb 10
+    string resb 8
     
 section .text
     global _start
 
+
+; print_number
+; parameter rdi: A decimal number less than length 8
+; return junk
+
+; print_string
+; parameter rdi: A string of printable ascii bytes
+; return junk
+
 _start:
-  mov r10, 3141592653589 ;move number to be stringed to reg
-  mov r11, 0 ; move 0 into r11
-  mov rax, r10 ; move into acc
-  mov rbx, 10
-  
+  mov rdi, msg
+  call print_string
+  mov rdi, msg1
+  call print_string
+  jmp exit
+
+print_number:
+   mov r10, rdi  ;move number to be stringed to reg
+   mov r11, 0 ; move 0 into r11
+   mov rax, r10 ; move into acc
+   mov rbx, 10
+   jmp start_loop
+   
+   
 start_loop: ; start loop
   xor rdx, rdx ; set rdx top bits to 0
   div rbx ; divide rax by 10 so rax holds quoitent and rdx holds remainder
@@ -24,6 +45,7 @@ start_loop: ; start loop
   mov rdi, 0
   mov rsi, r11
   dec rsi 
+  jmp reverse_loop
   
 reverse_loop:
   cmp rdi, rsi
@@ -44,7 +66,32 @@ done_reverse:
   mov rsi, string
   mov rdx, r11
   syscall
-  jmp exit
+  ret
+
+   
+strlen:
+    xor rax, rax
+    jmp loop
+
+loop:
+   mov dl, byte [rdi]
+   test dl, dl
+   jz found_null
+   inc rdi
+   inc rax
+   jmp loop
+
+found_null:
+    ret
+    
+print_string:
+    mov rsi, rdi
+    call strlen
+    mov rdx, rax
+    mov rax, 1
+    mov rdi, 1
+    syscall
+    ret
 
 exit:
    mov rax, 60
