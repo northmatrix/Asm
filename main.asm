1bf7fe1fad030bb0178a4c6efd494900bf239105
North Matrix <contact@northmatrix.co.uk>
2025-02-22 02:09:01 +0000

    wuff

--- main.asm
+++ main.asm
@@ -1,17 +1,61 @@
 .section .data
-     msg:  .asciz "Hello\n" 
+banner:  .asciz "A text adventure game made by nm\n" 
+p1: .asciz "Your finally awake... \n"
+.section .bss 
+name: .skip 128
 
 .section .text
     .globl _start
+    .globl write
+    .global printstr
+    .globl getstr
+    .globl strlen
 
-
-_start:
+write: 
     mov $1, %rax
     mov $1, %rdi
-    lea msg(%rip), %rsi
-    mov $6, %rdx
     syscall
+    ret
 
+getstr: 
+     mov $0, %rax
+     mov $0, %rdi
+     syscall 
+     ret
+
+strlen:
+    xor %rax, %rax        # Clear %rax to store the length
+.loop:
+    movb (%rdi), %cl      # Load byte from the string into %al
+    test %cl, %cl         # Test if it's the null terminator (0)
+    jz .done              # If null terminator, we are done
+    inc %rdi              # Move to the next byte in the string
+    inc %rax              # Increment the length counter
+    jmp .loop             # Repeat the loop
+.done:
+    ret                   # Return the length in %rax
+
+printstr:
+    mov %rsi,  %rdi 
+    call strlen 
+    mov %rax, %rdx 
+    call write
+    ret
+
+end:
     mov $60, %rax
     xor %rdi, %rdi
     syscall
+
+_start:
+    lea banner(%rip), %rsi
+    call printstr
+
+    lea p1(%rip), %rsi
+    call printstr
+
+    call end
+    
+
+
+
