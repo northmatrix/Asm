1d054ba67b534ebca876d099d0dd4eb64efd94a1
North Matrix <contact@northmatrix.co.uk>
2025-02-23 04:05:52 +0000

    added int to str, fixed up printstr and newline char

--- main.asm
+++ main.asm
@@ -1,8 +1,13 @@
 .section .data
 banner:  .asciz "A text adventure game made by nm\n" 
 p1: .asciz "Your finally awake... \n"
+h1: .asciz "Your health is: "
+
+newline: .asciz "\n"
+
 .section .bss 
 name: .skip 128
+buffer: .skip 128 
 
 .section .text
     .globl _start
@@ -23,6 +28,7 @@ getstr:
      syscall 
      ret
 
+# 1 Arg %RDI stores the pointer to string
 strlen:
     xor %rax, %rax        # Clear %rax to store the length
 .loop:
@@ -42,16 +48,69 @@ printstr:
     call write
     ret
 
+
+
+# 1 Arg %RDI: 64 bit int
+numbertostr:
+    mov %rdi, %rax # rdx:rax is the divide
+    xor %rdx, %rdx
+    mov $10, %rcx
+    mov $0, %rdi
+    lea buffer(%rip), %rsi  # %rsi is now buffer[0]
+.start_loop:
+    div %rcx  # this divides rax by rcx ( 10 ) and stores the remainder:quotient in rdx:rax
+    add $48, %dl  # dl is lower 8 bits of rdx so we add 48 to make ascii
+    movb %dl, (%rsi,%rdi)   # so now rsi  and rdx is a sum into rax wich should be 0 so buffer[0 + rdi] = dl 
+    test %rax, %rax
+    jz .finished 
+    inc %rdi
+    xor %rdx, %rdx
+    jmp .start_loop
+.finished:
+    ## it works up to this  point we just need to swap the chars around so 321 becomes 123 and 1234 becomes 4321
+    lea buffer(%rip), %rdi
+    call strlen
+    dec %rax
+    mov $0, %rsi
+    lea buffer(%rip), %rdx
+.start_swap_loop:
+    cmp  %rax, %rsi
+    jge .done_swap
+    movb (%rdx,%rsi), %cl
+    movb (%rdx,%rax), %bl
+    movb %bl, (%rdx,%rsi)
+    movb %cl, (%rdx,%rax)
+    inc %rsi
+    dec %rax
+    jmp .start_swap_loop
+
+.done_swap:
+    ret
+
+
 end:
     mov $60, %rax
     xor %rdi, %rdi
     syscall
 
 _start:
+    mov $100, %r8
+
     lea banner(%rip), %rsi
     call printstr
 
     lea p1(%rip), %rsi
+    call printstr
+
+    lea h1(%rip), %rsi
+    call printstr
+
+    mov %r8, %rdi
+    call numbertostr 
+    lea buffer(%rip), %rsi 
+    call printstr
+
+    lea newline(%rip), %rsi
     call printstr
 
     call end
